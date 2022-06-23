7a9af26bf550785cda6fdeac5d14e165043a0971
vinivasq <vinivasq@hotmail.com>
2022-06-22 21:36:16 -0300

    Ship triggers

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    
+    void OnCollisionEnter(Collision other) 
+    {
+        Debug.Log($"{this.name} ---Collided with--- {other.gameObject.name}");
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        Debug.Log($"{this.name} ***Triggered*** {other.gameObject.name}");
+    }
+
+}
