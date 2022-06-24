30ce9b582f92eb2ca260a559df426af86c5bc388
vinivasq <vinivasq@hotmail.com>
2022-06-24 18:47:43 -0300

    Scene Management

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    
+
     void OnCollisionEnter(Collision other) 
     {
         Debug.Log($"{this.name} ---Collided with--- {other.gameObject.name}");
@@ -13,6 +14,8 @@ public class CollisionHandler : MonoBehaviour
     void OnTriggerEnter(Collider other) 
     {
         Debug.Log($"{this.name} ***Triggered*** {other.gameObject.name}");
+        SceneManager.LoadScene(0);
+
     }
 
 }
