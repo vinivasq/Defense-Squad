a87160f5d8c5ba37d894b1d5200a7836e55f20cf
vinivasq <vinivasq@hotmail.com>
2022-06-30 22:05:20 -0300

    Score Interface

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] GameObject enemyCrashVFX;
+    [SerializeField] GameObject enemyExplosionVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
     [SerializeField] int addToScore = 2;
+    [SerializeField] int hitsToDie = 3;
+
+    int hitCount = 0;
     ScoreHandler updateScore;
 
     void Start() 
     {
+        
         updateScore = FindObjectOfType<ScoreHandler>();
     }
 
     void OnParticleCollision(GameObject other)
     {
         IncreaseScore();
+        
         KillEnemy();
     }
 
@@ -27,9 +33,13 @@ public class Enemy : MonoBehaviour
 
     void KillEnemy()
     {
-        GameObject vfx = Instantiate(enemyCrashVFX, transform.position, Quaternion.identity); // .identity keeps its original rotation values   
+        GameObject vfx = Instantiate(enemyExplosionVFX, transform.position, Quaternion.identity); // .identity keeps its original rotation values   
         vfx.transform.parent = parent;
-        Destroy(gameObject);
+        hitCount ++ ;  //ver video aula e tentar resolver o bug que o inimigo nave esta explodindo ao runtime
+        if (hitCount >= hitsToDie)
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
--- Assets/Scripts/ScoreHandler.cs
+++ Assets/Scripts/ScoreHandler.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreHandler : MonoBehaviour
 {
     int score;
+    TMP_Text scoreText;
+
+    void Start() 
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "SCORE: 000000";   
+    }
 
     public void UpdateScore (int amountToIncrease)
     {
         score += amountToIncrease;
-        Debug.Log($"Your Score: {score} ");
+        scoreText.text = amountToIncrease.ToString();
+        scoreText.text = $"SCORE: {score}";
     }
 
 
